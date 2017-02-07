0f0a7b873bd60e785a3a991216f6758bb478a5d8
shenlim <leaveslm@foxmail.com>
2017-02-07 17:44:46 +0800

    Chapter_03 profiles init commit

---
+++ Chapter_03/profiles/src/main/java/com/myapp/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package com.myapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    @Profile("dev")
+    //在Spring 3.1中，只能在类级别上使用@Profile注解；从Spring 3.2开始，可以在方法级别上使用@Profile注解
+    public DataSource embeddedDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
+    public DataSource jndiDataSource() {
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(DataSource.class);
+        return (DataSource) jndiObjectFactoryBean.getObject();
+    }
+
+}
